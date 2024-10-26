b5aa8c10e179df69a98b7c75a363a4a35785da98
Salih Çağlar <111198755+salih4187@users.noreply.github.com>
2024-10-26 23:54:31 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,8 +83,8 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Shims
 TARGET_LD_SHIM_LIBS += \
-    /vendor/bin/gpsd|/vendor/lib/libshim_gps.so \
-    /system/vendor/lib/libsec-ril.so|libsec-ril_shim.so
+    /vendor/bin/gpsd|/vendor/lib/libshim_gpsd.so \
+    /vendor/lib/libsec-ril.so|libshim_atomic.so
 
 # Camera: portrait orientation
 BOARD_CAMERA_FRONT_ROTATION := 270
