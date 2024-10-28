2b1900576cbdb193d64d7a33f19755735cb0b223
Salih Çağlar <111198755+salih4187@users.noreply.github.com>
2024-10-29 01:05:28 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -22,6 +22,10 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_AAPT_CONFIG := normal
 
 # Audio
+PRODUCT_PACKAGES += \
+    libtinyxml \
+    libtinyalsa
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml \
     $(LOCAL_PATH)/configs/audio/ysound.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/ysound.xml
