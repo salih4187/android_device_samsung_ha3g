7398b6d78f50da4e613f43a16059c2e4cf4d04e0
Salih Çağlar <salihcaglar4187@gmail.com>
2026-03-03 00:32:20 +0300

    ha3g: Update Ramdisk * Drop module eng tags * Move init files to inside of etc directory * Copy init files to /system/vendor/etc/init/hw during build * Drop deprecated and duplicate things * Update touchkey permissions for keydisabler

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -2,16 +2,16 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE        := init.target.rc
-LOCAL_MODULE_TAGS   := optional eng
+LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := ETC
-LOCAL_SRC_FILES     := init.target.rc
-LOCAL_MODULE_PATH   := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES     := etc/init.target.rc
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.baseband.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= init.baseband.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES		:= etc/init.baseband.rc
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
