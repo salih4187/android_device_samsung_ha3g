63423906d78c31117466bdd82967d31054d2af79
Salih Çağlar <salihcaglar4187@gmail.com>
2026-03-03 01:30:01 +0300

    ha3g: Build libcec for HDMI

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,9 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 # Bootloader
 TARGET_OTA_ASSERT_DEVICE := ha3g
 
+# CEC
+BOARD_USES_CEC := true
+
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_deathly_ha3g_defconfig
 
